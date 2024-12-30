1c68fd9025dd5a754cb5d181bd140cc20d0216e4
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-12-30 23:15:33 +0600

    added sign in ui design

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,10 +1,51 @@
-import { View, Text } from "react-native";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const SignIn = () => {
+  const handleLogin = () => {};
   return (
-    <View>
-      <Text>SignIn </Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-[60%]"
+          resizeMode="contain"
+        />
+
+        <View className="px-10">
+          <Text className="text-base text-center uppercase font-rubik text-black-200">
+            Welcome to ReState
+          </Text>
+          <Text className="text-2xl font-rubik-bold text-black-300 text-center mt-2">
+            Let's Get You Closer to {"\n"}
+            <Text className="text-primary-300">Your Ideal Home</Text>
+          </Text>
+
+          <Text className="text-lg font-rubik text-black-200 text-center mt-12">
+            Login to ReState with Google
+          </Text>
+
+          <TouchableOpacity
+            className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5"
+            onPress={handleLogin}
+          >
+            <View className="flex flex-row items-center justify-center">
+              <Image
+                source={icons.google}
+                className="size-5"
+                resizeMode="contain"
+              />
+
+              <Text className="text-lg font-rubik-medium text-black-300 ml-2">
+                Continue with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
